82eb7d6c8fb2de6b853a6697254d68ff8f9037ff
KiranPatil10 <141115928+KiranPatil10@users.noreply.github.com>
2024-07-25 14:50:03 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treate all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Kiran");
+
+let name = "Kiran"
+let age = 21
+let isLoggedIn = false
+
+
+// PREMITIVE DATATYPES
+// number => 2 to power of 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof(null));
